a520a9f761e46a7f3334025422943b1837e59cf4
王星 <wx1620@163.com>
2019-05-05 23:30:31 +0800

    静态服务器

--- newServer.js
+++ newServer.js
@@ -1,14 +1,32 @@
 var http=require('http');
+var url=require('url');
+var fs=require('fs');
+var path=require('path');
+function staticRoot (staticPath,req,res) {
+
+    var pathObj=url.parse(req.url,true);
+    if(pathObj.pathname==='/'){
+        pathObj.pathname+='views/index.html';
+    }
+    var filePath=path.join(staticPath,pathObj.pathname);
+    fs.readFile(filePath,'binary',function (err,fileContent) {
+       if(err){
+           console.log('404');
+           res.writeHead(404,'not found');
+           res.end('<h1>404 not found</h1>');
+       }else{
+           res.writeHead(200,'OK');
+           res.write(fileContent,'binary');
+           res.end();
+       }
+    });
+    //var fileContent=fs.readFileSync(filePath,'binary');
+}
 var server=http.createServer(function (request,response) {
-    response.setHeader('Content-Type','text/css;charset=utf-8');
-    //response.writeHead(200,  {'Content-Type':  'text/html;  charset=utf-8'});
-    response.writeHead(200,'haha');
-    response.write('<html><head><meta charset="utf-8"/></head>');
-    response.write('<body>');
-    response.write('<h1>你好</h1>');
-    response.write('</body>');
-    response.write('</html>');
-    response.end();
+    if(request.url!=="/favicon.ico"){  //清除第2此访问
+        console.log(__dirname);
+        staticRoot(__dirname,request,response);
+    }
 });
 console.log('Server  running  at  http://127.0.0.1:9000/');
 server.listen(9000);
