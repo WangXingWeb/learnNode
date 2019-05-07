148d1598d67ef0a1a0a3224688c3fa83e3291e1c
王星 <wx1620@163.com>
2019-05-07 23:52:31 +0800

    静态+动态服务器

--- newServer.js
+++ newServer.js
@@ -2,6 +2,25 @@ var http=require('http');
 var url=require('url');
 var fs=require('fs');
 var path=require('path');
+
+var routes={
+    '/a':function (req,res) {
+        console.log('a');
+        res.end(JSON.stringify(req.query));
+    },
+    '/b':function (req,res) {
+        res.end('match /b');
+    },
+    '/a/c':function (req,res) {
+        res.end('match /a/c');
+    },
+    '/search':function (req,res) {
+        console.log(req.body);
+        console.log('$$$$$$$$$$$');
+        res.end('username='+req.body.username+',password='+req.body.password);
+    }
+}
+
 function staticRoot (staticPath,req,res) {
     var pathObj=url.parse(req.url,true);
     if(pathObj.pathname==='/'){
@@ -12,7 +31,6 @@ function staticRoot (staticPath,req,res) {
     fs.readFile(filePath,'binary',function (err,fileContent) {
        if(err){
            console.log('404');
-           console.log('kkkk');
            res.writeHead(404,'not found');
            res.end('<h1>404 not found</h1>');
        }else{
@@ -23,10 +41,35 @@ function staticRoot (staticPath,req,res) {
     });
     //var fileContent=fs.readFileSync(filePath,'binary');
 }
+function parseBody(body){
+    console.log(body);
+    var obj={};
+    body.split('&').forEach(function (str) {
+        obj[str.split('=')[0]]=str.split('=')[1];
+    })
+    return obj;
+}
 var server=http.createServer(function (request,response) {
     if(request.url!=="/favicon.ico"){  //清除第2此访问
-        staticRoot(__dirname,request,response);
+        var pathObj=url.parse(request.url,true);
+        var handleFn=routes[pathObj.pathname];
+
+        if(handleFn){
+            request.query=pathObj.query;
+            var body='';
+            request.on('data',function (chunk) {
+                body+=chunk;
+            }).on('end',function () {
+                request.body=parseBody(body);
+                handleFn(request,response);
+            })
+        }else{
+            staticRoot(__dirname,request,response);
+        }
+
+
     }
 });
+
 console.log('Server  running  at  http://127.0.0.1:9000/');
 server.listen(9000);
